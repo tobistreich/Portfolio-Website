9415bceabd7510eb03915fcb583aadfe832a2e84
tobistreich <tobias.streich@sidion.de>
2023-12-05 08:57:42 +0100

    index.html styling, refactoringtechnologien.html init

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Dec 15, 2023 15:30:00").getTime();
-// Update the count down every 1 second
-var x = setInterval(function () {
-    // Get today's date and time
-    var now = new Date().getTime();
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-    // Time calculations for days, hours, minutes, and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    // Display the result in the element with id="countdown"
-    var countdownElement = document.getElementById("demo");
-    if (countdownElement) {
-        countdownElement.innerHTML = days + "d " + hours + "h "
-            + minutes + "m " + seconds + "s ";
-    }
-    // If the count down is finished, write some text
-    if (distance < 0) {
-        clearInterval(x);
+function createCountDown(endDate) {
+    // Set the date we're counting down to
+    var countDownDate = new Date(endDate).getTime();
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+        // Get today's date and time
+        var now = new Date().getTime();
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+        // Time calculations for days, hours, minutes, and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Display the result in the element with id="countdown"
+        var countdownElement = document.getElementById('demo');
         if (countdownElement) {
-            countdownElement.innerHTML = "Ungültig";
+            countdownElement.innerHTML =
+                days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
         }
-    }
-}, 1000);
+        // If the count down is finished, create a new countdown
+        if (distance < 0) {
+            clearInterval(x);
+            // Set a new end date (you can customize this date)
+            var newEndDate = new Date('March 23, 2024 15:00:00').getTime();
+            createCountDown(newEndDate);
+        }
+    }, 1000);
+}
+createCountDown('Dec 15, 2023 15:30:00');
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-
-
+function createCountDown(endDate) {
     // Set the date we're counting down to
-    var countDownDate = new Date("Dec 15, 2023 15:30:00").getTime();
-    // Update the count down every 1 second
-    var x = setInterval(function() {
+    var countDownDate = new Date(endDate).getTime();
 
+    // Update the count down every 1 second
+    var x = setInterval(function () {
         // Get today's date and time
         var now = new Date().getTime();
 
@@ -13,24 +12,27 @@
 
         // Time calculations for days, hours, minutes, and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var hours = Math.floor(
+            (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+        );
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         // Display the result in the element with id="countdown"
-        var countdownElement = document.getElementById("demo");
+        var countdownElement = document.getElementById('demo');
         if (countdownElement) {
-            countdownElement.innerHTML = days + "d " + hours + "h "
-            + minutes + "m " + seconds + "s ";
+            countdownElement.innerHTML =
+                days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
         }
 
-        // If the count down is finished, write some text
+        // If the count down is finished, create a new countdown
         if (distance < 0) {
             clearInterval(x);
-            if (countdownElement) {
-                countdownElement.innerHTML = "Ungültig";
-            }
+            // Set a new end date (you can customize this date)
+            var newEndDate = new Date('March 23, 2024 15:00:00').getTime();
+            createCountDown(newEndDate);
         }
     }, 1000);
+}
 
-
+createCountDown('Dec 15, 2023 15:30:00');
